04d0e4d7dbf40b912611b08fa09d457cf60c466a
GitHub Action <action@github.com>
2024-08-16 18:27:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/jd4n14/ttplug/MyBundle.kt
+++ src/main/kotlin/com/github/jd4n14/ttplug/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jd4n14.ttplug
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jd4n14/ttplug/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jd4n14/ttplug/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jd4n14.ttplug.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
